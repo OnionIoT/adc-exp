ad6e29173e17a023f9c530a50346ef73d3db1b78
greenbreakfast <lazardemin@gmail.com>
2017-10-12 11:41:44 -0400

    implemented reading input value from all of the input channels

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # adc-exp
 Software for the Onion ADC Expansion
+
+## Installation 
+
+Packages required by the `adc-exp.sh` script:
+
+```
+opkg update
+opkg install bc coreutils-sleep
+```
+
--- adc-exp.sh
+++ adc-exp.sh
@@ -15,6 +15,17 @@ deviceChannel=0
 deviceAddrSwitch0=0x4a
 deviceAddrSwitch1=0x48
 
+deviceConfigReg="0x01"
+deviceConversionReg="0x00"
+
+deviceInputMuxMask=0x8fff
+deviceInputMuxChannel0=0x4000
+deviceInputMuxChannel1=0x5000
+deviceInputMuxChannel2=0x6000
+deviceInputMuxChannel3=0x7000
+
+deviceConversionDelay="0.008"
+
 # usage
 Usage () {
         echo ""
@@ -33,7 +44,8 @@ Usage () {
 
 # initialize the expansion
 initializeExp () {
-	i2cset -y 0 $deviceAddr 0x01 0x8300 w
+	# writing 0x8083 to config register
+	i2cset -y 0 $deviceAddr $deviceConfigReg 0x8300 w
 }
 
 # set the device address and channel based on input 
@@ -57,14 +69,50 @@ decodeChannel () {
 	fi
 }
 
-# read the adc value
-#	argument1 - AINx channel
-readAdcValue () {
-	# TODO: change input channel reading based on inputChannel
+# program ADS chip to read a specific channel
+#	argument1 - ADS channel (0-3)
+# TODO: add automatic initialization if required to this function
+setChannel () {
+	# read current configuration register value
+	config=$(i2cget -y 0 $deviceAddr $deviceConfigReg w | sed -e 's/0x\(..\)\(..\)/0x\2\1/')
+	# mask out the input multiplexor config
+	config=$(printf "0x%04x\n" $(($config & $deviceInputMuxMask)))
 	
+	# set the input value
+	case "$1" in
+        0)
+        	muxMask=deviceInputMuxChannel0
+        ;;
+        1)
+        	muxMask=deviceInputMuxChannel1
+        ;;
+        2)
+        	muxMask=deviceInputMuxChannel2
+        ;;
+        3)
+        	muxMask=deviceInputMuxChannel3
+        ;;
+    esac
+    # apply the input value to the register value
+    config=$(printf "0x%04x\n" $(($config | $muxMask)))
+	
+	# flip the bytes and write to the config register
+	config=$(echo "$config" | sed -e 's/0x\(..\)\(..\)/0x\2\1/' )
+	i2cset -y 0 $deviceAddr $deviceConfigReg $config w
+	echo "wrote: i2cset -y 0 $deviceAddr $deviceConfigReg $config w" 
+	
+	# wait for the conversion delay before reading the conversion register
+	/usr/bin/sleep $deviceConversionDelay
+}
+
+# read the adc value
+#	argument1 - ADS device channel (0-3)
+readAdcValue () {
+	# set the input channel mux
+	#setChannel $1
 	
 	# read adc 16 bit value
-	adcVal=$(($(i2cget -y 0 $deviceAddr 0x00 w | sed -e 's/0x\(..\)\(..\)/0x\2\1/')/1))
+	adcVal=$(($(i2cget -y 0 $deviceAddr $deviceConversionReg w | sed -e 's/0x\(..\)\(..\)/0x\2\1/')/1))
 	
 	# check if negative
 	signChangeVal=$((0x8000/1))
@@ -136,16 +184,18 @@ fi
 
 
 # decode the device and channel 
+#	this sets global variables $deviceAddr and $deviceChannel
 decodeChannel $switchVal $inputChannel
 echo "decodeChannel done: deviceAddr = $deviceAddr, deviceChannel = $deviceChannel"
-exit
 
 # initialize the expansion
 if [ $bInit == 1 ]; then
 	initializeExp
 fi
 
-val=$(readAdcValue)
+setChannel $deviceChannel
+
+val=$(readAdcValue $deviceChannel)
 echo $val
 
 
