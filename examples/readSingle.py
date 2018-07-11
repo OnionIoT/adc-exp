44fe52d2525dd5741774c40a74b2c38e38a836c1
greenbreakfast <lazardemin@gmail.com>
2018-07-11 16:06:46 -0400

    added adc exp python module, added example python code

---
+++ examples/readSingle.py
@@ -0,0 +1,35 @@
+# Simple demo of reading each analog input from the ADS1x15 and printing it to
+# the screen.
+# Author: Tony DiCola
+# License: Public Domain
+import time
+
+# Import the ADS1x15 module.
+from AdcExp import AdcExp
+
+
+# Create an ADS1115 ADC (16-bit) instance.
+adc = AdcExp()
+
+# Note you can change the I2C address from its default (0x48), and/or the I2C
+# bus by passing in these optional parameters:
+#adc = Adafruit_ADS1x15.ADS1015(address=0x49, busnum=1)
+
+# Choose a gain of 1 for reading voltages from 0 to 4.09V.
+# Or pick a different gain to change the range of voltages that are read:
+#  - 2/3 = +/-6.144V
+#  -   1 = +/-4.096V
+#  -   2 = +/-2.048V
+#  -   4 = +/-1.024V
+#  -   8 = +/-0.512V
+#  -  16 = +/-0.256V
+# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
+GAIN = 2/3
+
+print('Reading ADC Expansion value')
+
+value = adc.read_adc(0, gain=GAIN)
+print('ADC = %f'%value)
+
+value = adc.read_voltage(0, gain=GAIN)
+print('Voltage = %.02f V'%value)
---
+++ python/__init__.py
@@ -0,0 +1,1 @@
+from .AdcExp import AdcExp
